67ff08c315564334a4e4367f1ef7806f7ae3ca07
RaviKumar757 <118956831+RaviKumar757@users.noreply.github.com>
2026-04-25 00:05:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function showURLLength() {
+  // Get current URL
+  const url = window.location.href;
+
+  // Get length of URL
+  const length = url.length;
+
+  // Show alert
+  alert("The length of the URL is: " + length);
+}
+
+// Call function (you can also attach to a button)
+showURLLength();
